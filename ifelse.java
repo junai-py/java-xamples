7f8415ff8b4657fd669105c2998cc56cba3eb22f
Arunjunai Rajan <41290080+junai96@users.noreply.github.com>
2019-03-22 17:11:16 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,12 @@
+public class Ifelse {
+    public static void main(String[] args){
+        int i=713;
+        if(i%2==0){
+            System.out.println("this is even");
+        }
+        else{
+            System.out.print("This is odd");
+        }
+
+    }
+}
